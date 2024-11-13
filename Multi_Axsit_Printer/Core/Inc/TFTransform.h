d943e23decbb4831493e9ad0b5dcbe6d4184ff07
weifu-yee <chenweifu9999@gmail.com>
2024-11-14 02:03:39 +0800

    Mod: First change to use Quarernion and then determine the TR matrix. || getJointPlanePoseInWorldFrame func. return the SPPose

--- Multi_Axsit_Printer/Core/Inc/TFTransform.h
+++ Multi_Axsit_Printer/Core/Inc/TFTransform.h
@@ -10,17 +10,17 @@
 
 #include <Eigen/Dense>
 #include <string>
-
+#include "stewart_platform.h"
 
 class TFTransformer {
 public:
     TFTransformer(); // 構造函數用於初始化變換矩陣
     Eigen::Affine3d getTransform(const std::string& from, const std::string& to) const;
-    Eigen::Affine3d getJointPlanePoseInWorldFrame() const;
+    SPPose getJointPlanePoseInWorldFrame() const; // 更新返回類型為 SPPose
 
     // 新增公共函數來更新非 const 變換矩陣
-	void setPartToNozzleTransform(double tx, double ty, double tz, double rx_degree, double ry_degree, double rz_degree);
-	void setWorkpieceOriginToPartTransform(double tx, double ty, double tz, double rx_degree, double ry_degree, double rz_degree);
+    void setPartToNozzleTransform(double tx, double ty, double tz, double rx_degree, double ry_degree, double rz_degree);
+    void setWorkpieceOriginToPartTransform(double tx, double ty, double tz, double rx_degree, double ry_degree, double rz_degree);
 
 private:
     // 變換矩陣的成員變數
@@ -32,6 +32,5 @@ private:
     Eigen::Affine3d part2nozzleTransform;
     Eigen::Affine3d workpiece_origin2partTransform;
 };
-
 
 #endif /* INC_TFTRANSFORM_H_ */
--- Multi_Axsit_Printer/Core/Src/TFTransform.cpp
+++ Multi_Axsit_Printer/Core/Src/TFTransform.cpp
@@ -15,29 +15,23 @@ double degreeToRad(double angle_in_degrees) {
 }
 
 TFTransformer::TFTransformer()
-    : word2nozzleTransform((Eigen::Translation3d(WORD2NOZZLE_TRANSLATION_X, WORD2NOZZLE_TRANSLATION_Y, WORD2NOZZLE_TRANSLATION_Z) *
-                            Eigen::AngleAxisd(degreeToRad(WORD2NOZZLE_ROTATION_X_DEGREE), Eigen::Vector3d::UnitX()) *
-                            Eigen::AngleAxisd(degreeToRad(WORD2NOZZLE_ROTATION_Y_DEGREE), Eigen::Vector3d::UnitY()) *
-                            Eigen::AngleAxisd(degreeToRad(WORD2NOZZLE_ROTATION_Z_DEGREE), Eigen::Vector3d::UnitZ())).matrix()),
+    : word2nozzleTransform(Eigen::Translation3d(WORD2NOZZLE_TRANSLATION_X, WORD2NOZZLE_TRANSLATION_Y, WORD2NOZZLE_TRANSLATION_Z) *
+                           (Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(WORD2NOZZLE_ROTATION_X_DEGREE), Eigen::Vector3d::UnitX())) *
+                            Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(WORD2NOZZLE_ROTATION_Y_DEGREE), Eigen::Vector3d::UnitY())) *
+                            Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(WORD2NOZZLE_ROTATION_Z_DEGREE), Eigen::Vector3d::UnitZ())))),
 
-      bed_surface2workpiece_originTransform((Eigen::Translation3d(BED_SURFACE2WORKPIECE_ORIGIN_TRANSLATION_X, BED_SURFACE2WORKPIECE_ORIGIN_TRANSLATION_Y, BED_SURFACE2WORKPIECE_ORIGIN_TRANSLATION_Z) *
-                                            Eigen::AngleAxisd(degreeToRad(BED_SURFACE2WORKPIECE_ORIGIN_ROTATION_X_DEGREE), Eigen::Vector3d::UnitX()) *
-                                            Eigen::AngleAxisd(degreeToRad(BED_SURFACE2WORKPIECE_ORIGIN_ROTATION_Y_DEGREE), Eigen::Vector3d::UnitY()) *
-                                            Eigen::AngleAxisd(degreeToRad(BED_SURFACE2WORKPIECE_ORIGIN_ROTATION_Z_DEGREE), Eigen::Vector3d::UnitZ())).matrix()),
+      bed_surface2workpiece_originTransform(Eigen::Translation3d(BED_SURFACE2WORKPIECE_ORIGIN_TRANSLATION_X, BED_SURFACE2WORKPIECE_ORIGIN_TRANSLATION_Y, BED_SURFACE2WORKPIECE_ORIGIN_TRANSLATION_Z) *
+                                            (Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(BED_SURFACE2WORKPIECE_ORIGIN_ROTATION_X_DEGREE), Eigen::Vector3d::UnitX())) *
+                                             Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(BED_SURFACE2WORKPIECE_ORIGIN_ROTATION_Y_DEGREE), Eigen::Vector3d::UnitY())) *
+                                             Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(BED_SURFACE2WORKPIECE_ORIGIN_ROTATION_Z_DEGREE), Eigen::Vector3d::UnitZ())))),
 
-      bed_joint_plane2bed_surfaceTransform((Eigen::Translation3d(BED_JOINT_PLANE2BED_SURFACE_TRANSLATION_X, BED_JOINT_PLANE2BED_SURFACE_TRANSLATION_Y, BED_JOINT_PLANE2BED_SURFACE_TRANSLATION_Z) *
-                                           Eigen::AngleAxisd(degreeToRad(BED_JOINT_PLANE2BED_SURFACE_ROTATION_X_DEGREE), Eigen::Vector3d::UnitX()) *
-                                           Eigen::AngleAxisd(degreeToRad(BED_JOINT_PLANE2BED_SURFACE_ROTATION_Y_DEGREE), Eigen::Vector3d::UnitY()) *
-                                           Eigen::AngleAxisd(degreeToRad(BED_JOINT_PLANE2BED_SURFACE_ROTATION_Z_DEGREE), Eigen::Vector3d::UnitZ())).matrix()) {
+      bed_joint_plane2bed_surfaceTransform(Eigen::Translation3d(BED_JOINT_PLANE2BED_SURFACE_TRANSLATION_X, BED_JOINT_PLANE2BED_SURFACE_TRANSLATION_Y, BED_JOINT_PLANE2BED_SURFACE_TRANSLATION_Z) *
+                                           (Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(BED_JOINT_PLANE2BED_SURFACE_ROTATION_X_DEGREE), Eigen::Vector3d::UnitX())) *
+                                            Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(BED_JOINT_PLANE2BED_SURFACE_ROTATION_Y_DEGREE), Eigen::Vector3d::UnitY())) *
+                                            Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(BED_JOINT_PLANE2BED_SURFACE_ROTATION_Z_DEGREE), Eigen::Vector3d::UnitZ())))) {
     // 初始化非 const 變換矩陣
-	part2nozzleTransform = Eigen::Translation3d(1.0, 1.0, 0.0) *
-	                       Eigen::AngleAxisd(degreeToRad(0.0), Eigen::Vector3d::UnitX()) *
-	                       Eigen::AngleAxisd(degreeToRad(0.0), Eigen::Vector3d::UnitY()) *
-	                       Eigen::AngleAxisd(degreeToRad(90.0), Eigen::Vector3d::UnitZ());
-	workpiece_origin2partTransform = Eigen::Translation3d(0.5, 0.5, 0.5) *
-	                                 Eigen::AngleAxisd(degreeToRad(0.0), Eigen::Vector3d::UnitX()) *
-	                                 Eigen::AngleAxisd(degreeToRad(45.0), Eigen::Vector3d::UnitY()) *
-	                                 Eigen::AngleAxisd(degreeToRad(0.0), Eigen::Vector3d::UnitZ());
+    setPartToNozzleTransform(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    setWorkpieceOriginToPartTransform(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 }
 
 Eigen::Affine3d TFTransformer::getTransform(const std::string& from, const std::string& to) const {
@@ -56,28 +50,45 @@ Eigen::Affine3d TFTransformer::getTransform(const std::string& from, const std::
     }
 }
 
-Eigen::Affine3d TFTransformer::getJointPlanePoseInWorldFrame() const {
-    // 取得從 WORLD_FRAME 到 BED_JOINT_PLANE_FRAME 的總變換
-    return getTransform("WORLD_FRAME", "NOZZLE_FRAME") *
-           getTransform("NOZZLE_FRAME", "PART_FRAME").inverse() *
-           getTransform("PART_FRAME", "WORKPIECE_ORIGIN_FRAME").inverse() *
-           getTransform("WORKPIECE_ORIGIN_FRAME", "BED_SURFACE_FRAME").inverse() *
-           getTransform("BED_SURFACE_FRAME", "BED_JOINT_PLANE_FRAME").inverse();
+SPPose TFTransformer::getJointPlanePoseInWorldFrame() const {
+    // 計算從 WORLD_FRAME 到 BED_JOINT_PLANE_FRAME 的總變換矩陣
+    Eigen::Affine3d total_transform = getTransform("WORLD_FRAME", "NOZZLE_FRAME") *
+                                      getTransform("PART_FRAME", "NOZZLE_FRAME").inverse() *
+                                      getTransform("WORKPIECE_ORIGIN_FRAME", "PART_FRAME").inverse() *
+                                      getTransform("BED_SURFACE_FRAME", "WORKPIECE_ORIGIN_FRAME").inverse() *
+                                      getTransform("BED_JOINT_PLANE_FRAME", "BED_SURFACE_FRAME").inverse();
+
+    // 提取平移分量
+    Eigen::Vector3d translation = total_transform.translation();
+
+    // 提取旋轉分量並轉換為歐拉角 (ZYX 順序)
+    Eigen::Vector3d euler_angles = total_transform.rotation().eulerAngles(2, 1, 0); // ZYX 順序
+
+    // 將位移和旋轉角度存入 SPPose 結構
+    SPPose pose;
+    pose.x = translation.x();
+    pose.y = translation.y();
+    pose.z = translation.z();
+    pose.phi = euler_angles[2];   // Roll (phi)
+    pose.theta = euler_angles[1]; // Pitch (theta)
+    pose.psi = euler_angles[0];   // Yaw (psi)
+
+    return pose;
 }
 
 // 用於設置 part2nozzleTransform 的新函數
 void TFTransformer::setPartToNozzleTransform(double tx, double ty, double tz, double rx_degree, double ry_degree, double rz_degree) {
-    part2nozzleTransform = Eigen::Translation3d(tx, ty, tz) *
-                           Eigen::AngleAxisd(degreeToRad(rx_degree), Eigen::Vector3d::UnitX()) *
-                           Eigen::AngleAxisd(degreeToRad(ry_degree), Eigen::Vector3d::UnitY()) *
-                           Eigen::AngleAxisd(degreeToRad(rz_degree), Eigen::Vector3d::UnitZ());
+    Eigen::Quaterniond rotation = Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(rx_degree), Eigen::Vector3d::UnitX())) *
+                                  Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(ry_degree), Eigen::Vector3d::UnitY())) *
+                                  Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(rz_degree), Eigen::Vector3d::UnitZ()));
+    part2nozzleTransform = Eigen::Translation3d(tx, ty, tz) * rotation;
 }
 
 // 用於設置 workpiece_origin2partTransform 的新函數
 void TFTransformer::setWorkpieceOriginToPartTransform(double tx, double ty, double tz, double rx_degree, double ry_degree, double rz_degree) {
-    workpiece_origin2partTransform = Eigen::Translation3d(tx, ty, tz) *
-                                     Eigen::AngleAxisd(degreeToRad(rx_degree), Eigen::Vector3d::UnitX()) *
-                                     Eigen::AngleAxisd(degreeToRad(ry_degree), Eigen::Vector3d::UnitY()) *
-                                     Eigen::AngleAxisd(degreeToRad(rz_degree), Eigen::Vector3d::UnitZ());
+    Eigen::Quaterniond rotation = Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(rx_degree), Eigen::Vector3d::UnitX())) *
+                                  Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(ry_degree), Eigen::Vector3d::UnitY())) *
+                                  Eigen::Quaterniond(Eigen::AngleAxisd(degreeToRad(rz_degree), Eigen::Vector3d::UnitZ()));
+    workpiece_origin2partTransform = Eigen::Translation3d(tx, ty, tz) * rotation;
 }
 
